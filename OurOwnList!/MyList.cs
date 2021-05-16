d50cf8de2a1719606092b1bfb7839f101f6c9b9f
maximklaiman@gmail.com <maximklaiman@gmail.com>
2021-05-16 10:18:51 -0700

    Add project files.

---
+++ OurOwnList!/MyList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OurOwnList_
+{
+
+    class MyList<T> where T : IComparable<T>
+    {
+        public int Count { get; private set; } = 0;
+        //fortesting
+     
+        T[] thelist = new T[10];
+        public int fulllength
+        {
+            get
+            {
+                return thelist.Length;
+            }
+        }
+        public T this[int index]
+        {
+            get
+            {
+                return thelist[index];
+            }
+            set
+            {
+                thelist[index] = value; 
+            }
+        }
+
+        public MyList()
+        {
+
+        }
+
+        public void Addtolist(T thingadding)
+        {
+            thelist[Count] = thingadding;
+            Count += 1;
+            if (Count == thelist.Length)
+            {
+                //addtothelistnewindexes
+
+                T[] temp = new T[thelist.Length+10];
+                for(int i = 0;i < thelist.Length; i++)
+                {
+                    temp[i] = thelist[i];
+                }
+                thelist = temp;
+
+
+            }
+        }
+
+        public void removefromlist(T thingremoved)
+        {
+            int hasithit = 0;
+            T[] temp = new T[thelist.Length];
+            for (int i = 0; i < Count; i++)
+            {
+                int x = 30;
+                int y = 20;
+
+                int comparison = x.CompareTo(y);
+                if (thelist[i].CompareTo(thingremoved) == 0)
+                {
+                    hasithit += 1;
+                }
+                else
+                {
+                    temp[i - hasithit] = thelist[i];
+                }
+            }
+            thelist = temp;
+        }
+
+        public void removeindex(int index)
+        {
+            int hasithit = 0;
+            T[] temp = new T[thelist.Length];
+            for (int i = 0; i < Count; i++)
+            {
+                if (i == index)
+                {
+                    hasithit += 1;
+                }
+                else
+                {
+                    temp[i-hasithit] = thelist[i];
+                }
+            }
+            thelist = temp;
+        }
+    }
+}
---
+++ OurOwnList!/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OurOwnList_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyList<int> listofbools = new MyList<int>();
+
+            for (int i = 0; i <= 25; i++)
+            {
+                listofbools.Addtolist(i);
+            }
+
+            listofbools.removefromlist(7);
+            
+            //listofbools.Addtolist();
+
+            for (int i = 0; i < listofbools.Count; i++)
+            {
+                Console.WriteLine(listofbools[i]);
+            }
+
+
+          
+        }
+    }
+}
